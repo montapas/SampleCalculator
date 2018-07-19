aa24c6793b644cfafc73bbcc1371f6421ac47b94
montapas <montapas@gmail.com>
2018-07-19 09:48:36 +0530

    sd
    sd

--- src/main/java/com/cts/examples/cal/ICalculator.java
+++ src/main/java/com/cts/examples/cal/ICalculator.java
@@ -7,7 +7,7 @@ public interface ICalculator {
 
 	int multiplication(int a, int b);
 
-	int divison(int a, int b) throws Exception;
+	int divison(int a, int b);
 
 	boolean equalIntegers(int a, int b);
 }
